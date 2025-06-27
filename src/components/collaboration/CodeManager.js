8ca992db07c7baafc17df538f50dc10b4744d34a
Harry Ye <harry.ye@mail.utoronto.ca>
2025-06-27 16:17:12 -0400

    remove unused files

--- src/components/collaboration/CodeManager.js
+++
@@ -1,9 +0,0 @@
-// This component has been unified with CodePalette into CodeManagement
-// Import CodeManagement with mode="management"
-import CodeManagement from './codePalette/CodeManagement.js';
-
-const CodeManager = (props) => {
-  return <CodeManagement {...props} mode="management" />;
-};
-
-export default CodeManager;
--- src/components/collaboration/codePalette/CodePalette.js
+++
@@ -1,3 +0,0 @@
-// This component has been unified with CodeManager into CodeManagement
-// Import CodeManagement for both selection and management modes
-export { default } from './CodeManagement.js';
