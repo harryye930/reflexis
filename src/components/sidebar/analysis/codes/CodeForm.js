fed0256732a7a0f3a978997b32ea912108895969
Harry Ye <harry.ye@mail.utoronto.ca>
2025-08-16 10:45:04 -0400

    double code label limit

--- src/components/sidebar/analysis/codes/CodeForm.js
+++ src/components/sidebar/analysis/codes/CodeForm.js
@@ -67,7 +67,7 @@ const CodeForm = ({
           onChange={(e) => setFormData(prev => ({ ...prev, label: e.target.value }))}
           className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           placeholder="e.g., Methodology"
-          maxLength={20}
+          maxLength={40}
           required
         />
       </div>
